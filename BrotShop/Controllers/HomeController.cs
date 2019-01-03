53d14126a356d1a9bac262ec22edccda1ce91b84
Mats Egnell <mattegol.egnell@gmail.com>
2019-01-03 15:16:50 +0100

    Adding a Controller

---
+++ BrotShop/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrotShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrotShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IGoodieRepository _goodieRepository;
+
+        public HomeController(IGoodieRepository goodieRepository)
+        {
+            _goodieRepository = goodieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_goodieRepository.GetAllGoodies());
+        }
+    }
+}
